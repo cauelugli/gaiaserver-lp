18d8aabf8cb2e14b2c5ca6d08d491ae923ce4461
cauelugli <cauec4@gmail.com>
2023-09-04 16:46:46 -0300

    job info enhanced

--- frontend/src/tables/JobTable.jsx
+++ frontend/src/tables/JobTable.jsx
@@ -119,20 +119,134 @@ export default function JobTable({ jobs, fetchData }) {
                       timeout="auto"
                       unmountOnExit
                     >
-                      <Box sx={{ m: 1, p: 4 }}>
-                        <Typography variant="h6" gutterBottom component="div">
-                          Detalhes
-                        </Typography>
+                      <Box sx={{ my: 4, px: 6 }}>
+                        <Typography variant="h6">Geral</Typography>
                         <Table size="small">
                           <TableHead>
                             <TableRow>
-                              <TableCell>Titulo</TableCell>
+                              <TableCell>
+                                <Typography
+                                  sx={{ fontSize: "14px", color: "#777" }}
+                                >
+                                  Título do Job
+                                </Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography
+                                  sx={{ fontSize: "14px", color: "#777" }}
+                                >
+                                  Solicitante
+                                </Typography>
+                              </TableCell>
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                            <TableRow>
+                              <TableCell align="left">
+                                <Typography>{job.title}</Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography>
+                                  {job.requester} ({job.customer.name})
+                                </Typography>
+                              </TableCell>
+                            </TableRow>
+                          </TableBody>
+                        </Table>
+                      </Box>
+                      <Box sx={{ my: 4, px: 6 }}>
+                        <Typography variant="h6">Departamento</Typography>
+                        <Table size="small">
+                          <TableHead>
+                            <TableRow>
+                              <TableCell align="left">
+                                <Typography
+                                  sx={{ fontSize: "14px", color: "#777" }}
+                                >
+                                  Nome do Departamento
+                                </Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography
+                                  sx={{ fontSize: "14px", color: "#777" }}
+                                >
+                                  Gerente
+                                </Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography
+                                  sx={{ fontSize: "14px", color: "#777" }}
+                                >
+                                  Colaborador Designado
+                                </Typography>
+                              </TableCell>
+                            </TableRow>
+                          </TableHead>
+                          <TableBody>
+                            <TableRow>
+                              <TableCell align="left">
+                                <Typography>{job.department.name}</Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography>{job.manager.name}</Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography>{job.worker.name}</Typography>
+                              </TableCell>
+                            </TableRow>
+                          </TableBody>
+                        </Table>
+                      </Box>
+                      <Box sx={{ my: 4, px: 6 }}>
+                        <Typography variant="h6">Orçamento</Typography>
+                        <Table size="small">
+                          <TableHead>
+                            <TableRow>
+                              <TableCell>
+                                <Typography
+                                  sx={{ fontSize: "14px", color: "#777" }}
+                                >
+                                  Nº do Orçamento
+                                </Typography>
+                              </TableCell>
+                              <TableCell>
+                                <Typography
+                                  sx={{ fontSize: "14px", color: "#777" }}
+                                >
+                                  Tipo de Serviço
+                                </Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography
+                                  sx={{ fontSize: "14px", color: "#777" }}
+                                >
+                                  Materiais
+                                </Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography
+                                  sx={{ fontSize: "14px", color: "#777" }}
+                                >
+                                  Valor Total
+                                </Typography>
+                              </TableCell>
                             </TableRow>
                           </TableHead>
                           <TableBody>
                             <TableRow>
                               <TableCell component="th" scope="row">
-                                <Typography>{job.title}</Typography>
+                                <Typography>{job.quoteNumber}</Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography>
+                                  {job.service.name}
+                                </Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography>{job.service.materials.length > 0 ? job.service.material : "Não há uso de Materiais"}</Typography>
+                              </TableCell>
+                              <TableCell align="left">
+                                <Typography>R${job.price}</Typography>
                               </TableCell>
                             </TableRow>
                           </TableBody>
@@ -140,13 +254,11 @@ export default function JobTable({ jobs, fetchData }) {
                         <Box sx={{ mt: 3, ml: "90%" }}>
                           <ModeEditIcon
                             cursor="pointer"
-                            option="delete"
                             onClick={() => handleOpenEdit(job)}
                             sx={{ color: "grey", mr: 2 }}
                           />
                           <DeleteIcon
                             cursor="pointer"
-                            option="delete"
                             onClick={() => handleConfirmDelete(job)}
                             sx={{ color: "#ff4444" }}
                           />
