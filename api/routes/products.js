68acfdaecc35e80c960f0dcefa2a6842a82ca8cd
cauelugli <cauec4@gmail.com>
2023-09-19 15:32:56 -0300

    AddMultipleProduct working

--- api/routes/products.js
+++ api/routes/products.js
@@ -14,13 +14,28 @@ router.get("/", async (req, res) => {
 
 // CREATE PRODUCT
 router.post("/", async (req, res) => {
-  const newProduct = new Product(req.body);
-  try {
-    const savedProduct = await newProduct.save();
-    res.status(200).json(savedProduct);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+  if (req.body.productList && req.body.productList.length > 0) {
+    try {
+      const savedProducts = [];
+      for (const productData of req.body.productList) {
+        const newProduct = new Product(productData);
+        const savedProduct = await newProduct.save();
+        savedProducts.push(savedProduct);
+      }
+      res.status(200).json(savedProducts);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  } else {
+    const newProduct = new Product(req.body);
+    try {
+      const savedProduct = await newProduct.save();
+      res.status(200).json(savedProduct);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   }
 });
 
@@ -31,7 +46,7 @@ router.delete("/:id", async (req, res) => {
     const deletedProduct = await Product.findByIdAndDelete(productId);
     res.status(200).json(deletedProduct);
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(500).json(err);
   }
 });
--- frontend/src/forms/add/AddMultipleProductForm.jsx
+++ frontend/src/forms/add/AddMultipleProductForm.jsx
@@ -10,6 +10,7 @@ import {
   DialogTitle,
   FormHelperText,
   Grid,
+  IconButton,
   InputAdornment,
   MenuItem,
   Select,
@@ -19,6 +20,7 @@ import {
 
 import FileUploadIcon from "@mui/icons-material/FileUpload";
 import DeleteIcon from "@mui/icons-material/Delete";
+import ClearIcon from '@mui/icons-material/Clear';
 
 const api = axios.create({
   baseURL: "http://localhost:3000/api",
@@ -30,14 +32,32 @@ export default function AddMultipleProductForm({
   fetchData,
   toast,
 }) {
-  const [name, setName] = React.useState("");
-  const [image, setImage] = React.useState("");
-  const [type, setType] = React.useState("");
-  const [model, setModel] = React.useState("");
-  const [size, setSize] = React.useState("");
-  const [groupingType, setGroupingType] = React.useState("Unidade");
-  const [buyValue, setBuyValue] = React.useState(0);
-  const [sellValue, setSellValue] = React.useState(0);
+  const [productList, setProductList] = React.useState([
+    {
+      name: "",
+      image: "",
+      type: "",
+      color: "",
+      model: "",
+      size: "",
+      groupingType: "Unidade",
+      buyValue: 0,
+      sellValue: 0,
+    },
+  ]);
+
+  const handleFieldChange = (index, fieldName, value) => {
+    const updatedList = [...productList];
+    updatedList[index] = {
+      ...updatedList[index],
+      [fieldName]: value,
+    };
+    setProductList(updatedList);
+  };
+
+  const handleAddProductLine = () => {
+    setProductList([...productList, { ...productList[0] }]);
+  };
 
   const convertImageToBase64 = (imageFile) => {
     return new Promise((resolve, reject) => {
@@ -62,20 +82,12 @@ export default function AddMultipleProductForm({
 
   const handleAdd = async (e) => {
     e.preventDefault();
-    const imageBase64 = await convertImageToBase64(image);
     try {
       const res = await api.post("/products", {
-        name,
-        image: imageBase64,
-        type,
-        model,
-        size,
-        groupingType,
-        buyValue,
-        sellValue,
+        productList,
       });
       if (res.data) {
-        toast.success("Produto Adicionado!", {
+        toast.success("Produtos Adicionados!", {
           closeOnClick: true,
           pauseOnHover: false,
           theme: "colored",
@@ -94,209 +106,247 @@ export default function AddMultipleProductForm({
     <form onSubmit={handleAdd}>
       <DialogTitle>Novos Produtos</DialogTitle>
       <DialogContent>
-        <Grid
-          container
-          sx={{ mt: 2 }}
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="center"
-        >
-          <Grid item>
-            <Typography sx={{ fontSize: 14 }}>Nome</Typography>
-            <TextField
-              size="small"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-              sx={{ width: 200 }}
-            />
-          </Grid>
-
-          <Grid item sx={{ ml: 1 }}>
-            <Typography sx={{ fontSize: 14 }}>Tipo</Typography>
-            <TextField
-              size="small"
-              value={type}
-              onChange={(e) => setType(e.target.value)}
-              required
-              variant="outlined"
-              sx={{ width: 100 }}
-            />
-          </Grid>
-
-          <Grid item sx={{ ml: 1 }}>
-            <Typography sx={{ fontSize: 14 }}>Tamanho</Typography>
-            <TextField
-              size="small"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-              required
-              variant="outlined"
-              sx={{ width: 100 }}
-            />
-          </Grid>
-
-          <Grid item sx={{ ml: 1 }}>
-            <Typography sx={{ fontSize: 14 }}>Cor</Typography>
-            <TextField
-              size="small"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-              required
-              variant="outlined"
-              sx={{ width: 100 }}
-            />
-          </Grid>
-
-          <Grid item sx={{ mx: 1 }}>
-            <Typography sx={{ fontSize: 14 }}>Modelo</Typography>
-            <TextField
-              size="small"
-              value={model}
-              onChange={(e) => setModel(e.target.value)}
-              required
-              variant="outlined"
-              sx={{ width: 100 }}
-            />
-          </Grid>
-
-          <Grid item>
-            <Typography sx={{ fontSize: 14 }}>Agrupamento</Typography>
-            <Select
-              size="small"
-              value={groupingType}
-              onChange={(e) => setGroupingType(e.target.value)}
-              required
-              sx={{ width: 110 }}
-            >
-              <MenuItem value="Unidade">
-                <Typography>Unidade</Typography>
-              </MenuItem>
-              <MenuItem value="Caixa">
-                <Typography>Caixa</Typography>
-              </MenuItem>
-              <MenuItem value="Kit">
-                <Typography>Kit</Typography>
-              </MenuItem>
-            </Select>
-          </Grid>
-          <Grid item sx={{ ml: 1 }}>
-            <Typography sx={{ fontSize: 14 }}>Valor de Compra</Typography>
-            <TextField
-              type="number"
-              size="small"
-              value={buyValue}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">R$</InputAdornment>
-                ),
-              }}
-              onChange={(e) => {
-                const inputValue = e.target.value;
-                if (inputValue >= 0) {
-                  setBuyValue(inputValue);
-                }
-              }}
-              required
-              variant="outlined"
-              sx={{ width: 120 }}
-            />
-          </Grid>
-
-          <Grid item sx={{ ml: 1 }}>
-            <Typography sx={{ fontSize: 14 }}>Valor de Venda</Typography>
-            <TextField
-              type="number"
-              size="small"
-              value={sellValue}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">R$</InputAdornment>
-                ),
-              }}
-              onChange={(e) => {
-                const inputValue = e.target.value;
-                if (inputValue >= 0) {
-                  setSellValue(inputValue);
-                }
-              }}
-              required
-              variant="outlined"
-              sx={{ width: 120 }}
-            />
-          </Grid>
-          <Grid item sx={{ ml: 0.5, mt: 2.5 }}>
-            {sellValue > 0 && buyValue > 0 && (
-              <Typography sx={{ fontSize: 12, color: "green" }}>
-                +{(((sellValue - buyValue) / buyValue) * 100).toFixed(2)}% por {groupingType}
-              </Typography>
-            )}
-          </Grid>
-        </Grid>
-
-        <Grid sx={{ mt: 2 }}>
-          <Typography>Imagem</Typography>
-          <div>
-            <input
-              type="file"
-              accept="image/*"
-              id="fileInput"
-              style={{ display: "none" }}
-              onChange={(e) => {
-                const selectedImage = e.target.files[0];
-                setImage(selectedImage);
-              }}
-              required
-            />
-            {!image && (
-              <label htmlFor="fileInput">
-                <Button
-                  variant="outlined"
-                  color="primary"
-                  component="span"
-                  size="small"
-                  startIcon={<FileUploadIcon />}
-                >
-                  Carregar Imagem
-                </Button>
-              </label>
-            )}
-          </div>
-        </Grid>
-
-        {image && (
+        {productList.map((product, index) => (
           <Grid
+            key={index}
+            id={`productLine${index}`}
             container
-            direction="column"
+            direction="row"
+            justifyContent="flex-start"
             alignItems="center"
-            justifyContent="center"
           >
+            {index !== 0 && (
+              <Grid item>
+                <IconButton
+                  sx={{ ml: -2 }}
+                  variant="contained"
+                  color="error"
+                  size="small"
+                  onClick={() => {
+                    setProductList((prevList) => prevList.filter((_, idx) => idx !== index));
+                  }}                >
+                  <ClearIcon />
+                </IconButton>
+              </Grid>
+            )}
             <Grid item>
-              <img
-                src={URL.createObjectURL(image)}
-                alt="Prévia da Imagem"
-                style={{
-                  marginTop: 20,
-                  maxWidth: "200px",
-                  maxHeight: "200px",
-                }}
+              {index === 0 && (
+                <Typography sx={{ fontSize: 14, ml:2 }}>Nome</Typography>
+              )}
+              <TextField
+                size="small"
+                value={product.name}
+                onChange={(e) =>
+                  handleFieldChange(index, "name", e.target.value)
+                }
+                required
+                sx={{ width: 200, ml: index === 0 ? 2 : 0}}
+              />
+            </Grid>
+            <Grid item sx={{ ml: 1 }}>
+              {index === 0 && (
+                <Typography sx={{ fontSize: 14 }}>Tipo</Typography>
+              )}
+              <TextField
+                size="small"
+                value={product.type}
+                onChange={(e) =>
+                  handleFieldChange(index, "type", e.target.value)
+                }
+                required
+                variant="outlined"
+                sx={{ width: 100 }}
+              />
+            </Grid>
+            <Grid item sx={{ ml: 1 }}>
+              {index === 0 && (
+                <Typography sx={{ fontSize: 14 }}>Tamanho</Typography>
+              )}
+              <TextField
+                size="small"
+                value={product.size}
+                onChange={(e) =>
+                  handleFieldChange(index, "size", e.target.value)
+                }
+                required
+                variant="outlined"
+                sx={{ width: 100 }}
+              />
+            </Grid>
+            <Grid item sx={{ ml: 1 }}>
+              {index === 0 && (
+                <Typography sx={{ fontSize: 14 }}>Cor</Typography>
+              )}
+              <TextField
+                size="small"
+                value={product.color}
+                onChange={(e) =>
+                  handleFieldChange(index, "color", e.target.value)
+                }
+                required
+                variant="outlined"
+                sx={{ width: 100 }}
+              />
+            </Grid>
+            <Grid item sx={{ mx: 1 }}>
+              {index === 0 && (
+                <Typography sx={{ fontSize: 14 }}>Modelo</Typography>
+              )}
+              <TextField
+                size="small"
+                value={product.model}
+                onChange={(e) =>
+                  handleFieldChange(index, "model", e.target.value)
+                }
+                required
+                variant="outlined"
+                sx={{ width: 100 }}
               />
             </Grid>
             <Grid item>
-              <FormHelperText>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  size="small"
-                  startIcon={<DeleteIcon />}
-                  onClick={() => setImage("")}
-                >
-                  Remover
-                </Button>
-              </FormHelperText>
+              {index === 0 && (
+                <Typography sx={{ fontSize: 14 }}>Agrupamento</Typography>
+              )}
+              <Select
+                size="small"
+                value={product.groupingType}
+                onChange={(e) =>
+                  handleFieldChange(index, "groupingType", e.target.value)
+                }
+                required
+                sx={{ width: 110 }}
+              >
+                <MenuItem value="Unidade">
+                  <Typography>Unidade</Typography>
+                </MenuItem>
+                <MenuItem value="Caixa">
+                  <Typography>Caixa</Typography>
+                </MenuItem>
+                <MenuItem value="Kit">
+                  <Typography>Kit</Typography>
+                </MenuItem>
+              </Select>
+            </Grid>
+            <Grid item sx={{ ml: 1 }}>
+              {index === 0 && (
+                <Typography sx={{ fontSize: 14 }}>Valor de Compra</Typography>
+              )}
+              <TextField
+                type="number"
+                size="small"
+                value={product.buyValue}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">R$</InputAdornment>
+                  ),
+                }}
+                onChange={(e) =>
+                  handleFieldChange(index, "buyValue", e.target.value)
+                }
+                required
+                variant="outlined"
+                sx={{ width: 120 }}
+              />
+            </Grid>
+            <Grid item sx={{ ml: 1 }}>
+              {index === 0 && (
+                <Typography sx={{ fontSize: 14 }}>Valor de Venda</Typography>
+              )}
+              <TextField
+                type="number"
+                size="small"
+                value={product.sellValue}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">R$</InputAdornment>
+                  ),
+                }}
+                onChange={(e) =>
+                  handleFieldChange(index, "sellValue", e.target.value)
+                }
+                required
+                variant="outlined"
+                sx={{ width: 120 }}
+              />
+            </Grid>
+            <Grid item sx={{ ml: 2, mb: index !== 0 && 2 }}>
+              {product.image ? (
+                <Grid container direction="row">
+                  <img
+                    src={
+                      product.image instanceof File
+                        ? URL.createObjectURL(product.image)
+                        : product.image
+                    }
+                    alt="Prévia da Imagem"
+                    style={{
+                      maxWidth: "60px",
+                      maxHeight: "60px",
+                    }}
+                  />
+                  <IconButton
+                    sx={{ ml: 1 }}
+                    variant="contained"
+                    color="error"
+                    size="small"
+                    onClick={() => handleFieldChange(index, "image", "")}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </Grid>
+              ) : (
+                <div>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    id={`fileInput${index}`}
+                    style={{ display: "none" }}
+                    onChange={async (e) => {
+                      const selectedImage = e.target.files[0];
+                      try {
+                        const imageBase64 = await convertImageToBase64(
+                          selectedImage
+                        );
+                        handleFieldChange(index, "image", imageBase64);
+                      } catch (error) {
+                        console.error(
+                          "Erro ao converter imagem para Base64:",
+                          error
+                        );
+                      }
+                    }}
+                    required
+                  />
+                  {!product.image && (
+                    <label htmlFor={`fileInput${index}`}>
+                      <Button
+                        sx={{ mt: 2 }}
+                        variant="outlined"
+                        color="primary"
+                        component="span"
+                        size="small"
+                        startIcon={<FileUploadIcon />}
+                      >
+                        Imagem
+                      </Button>
+                    </label>
+                  )}
+                </div>
+              )}
             </Grid>
           </Grid>
-        )}
+        ))}
+
+        <Grid
+          id="addProductLine"
+          container
+          sx={{ my: 2 }}
+          justifyContent="flex-end"
+        >
+          <Button variant="contained" onClick={handleAddProductLine}>
+            + NOVO
+          </Button>
+        </Grid>
       </DialogContent>
       <DialogActions>
         <Button type="submit" variant="contained" color="success">
