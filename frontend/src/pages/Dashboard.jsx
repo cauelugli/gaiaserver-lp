757103a96c39e67c6301c423ce0115bc73acc3bf
cauelugli <cauec4@gmail.com>
2023-11-03 07:56:18 -0300

    dashboard with user info

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -10,12 +10,16 @@ const Dashboard = ({ user }) => {
       <Typography sx={{ my: 3 }}>
         Hellow, <strong>{user.name}</strong> !
       </Typography>
+      <Typography sx={{ my: 1 }}>Departamento: {user.department.name}</Typography>
+      <Typography sx={{ my: 1 }}>Acesso: {user.role}</Typography>
+      <Typography sx={{ my: 1 }}>Ocupação: {user.position}</Typography>
+      <Typography sx={{ my: 1 }}>Usuário: {user.username}</Typography>
+
       <img
         src={`http://localhost:3000/static${user.image}`}
         alt="Logo do Tenant"
         style={{
           width: "8%",
-          marginLeft: 30,
           marginTop: 2,
           marginBottom: 2,
           cursor: "pointer",
