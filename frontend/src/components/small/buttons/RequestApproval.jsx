92224fdf46192c6d92010b5b707e8cebbe68b90e
cauelugli <cauec4@gmail.com>
2023-12-26 09:06:23 -0300

    created Button

---
+++ frontend/src/components/small/buttons/RequestApproval.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import * as React from "react";
+
+import { IconButton, Tooltip, Typography } from "@mui/material";
+
+export default function RequestApproval() {
+  return (
+    <Tooltip
+      title={
+        <Typography sx={{ fontSize: 12 }}>
+         Clique para solicitar a Aprovação do Gerente do departamento
+        </Typography>
+      }
+    >
+      <IconButton size="small" onClick={() => alert("clicked")}>
+        <Typography
+          sx={{
+            color: "white",
+            fontWeight: "bold",
+            fontSize: 12,
+            px: 1,
+            ml: 0.5,
+            backgroundColor: "#32aacd",
+          }}
+        >
+          !
+        </Typography>
+      </IconButton>
+    </Tooltip>
+  );
+}
