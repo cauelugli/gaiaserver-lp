761995b8718d4f55791e207e6d6f8b891a2d734d
cauelugli <cauec4@gmail.com>
2024-01-30 17:46:09 -0300

    Project review table almost fully working

--- frontend/src/components/ProjectReviewTable.jsx
+++ frontend/src/components/ProjectReviewTable.jsx
@@ -1,7 +1,28 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { Table, TableCell, TableHead, TableRow } from "@mui/material";
 import React from "react";
+
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Avatar,
+  Box,
+  Button,
+  Collapse,
+  Grid,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const ProjectReviewTable = ({
   type,
@@ -16,47 +37,169 @@ const ProjectReviewTable = ({
   description,
 }) => {
   return (
-    <>
-      <Table>
-        <TableHead>
-          <TableCell>Tipo</TableCell>
-          <TableCell>Cliente</TableCell>
-          <TableCell>Tipo de Cliente</TableCell>
-        </TableHead>
-        <TableRow>
-          <TableCell>{type}</TableCell>
-          <TableCell>{customer}</TableCell>
-          <TableCell>{customerType}</TableCell>
-        </TableRow>
-      </Table>
-      <Table>
-        <TableHead>
-          <TableCell>Nome do Projeto</TableCell>
-          <TableCell>Descrição</TableCell>
-          <TableCell>Valor</TableCell>
-          <TableCell>Departamento Principal</TableCell>
-          <TableCell>Departamentos Secundários</TableCell>
-        </TableHead>
-        <TableRow>
-          <TableCell>{name}</TableCell>
-          <TableCell>{description}</TableCell>
-          <TableCell>{price}</TableCell>
-          <TableCell>{mainDepartment}</TableCell>
-          <TableCell>{selectedDepartments}</TableCell>
-        </TableRow>
-      </Table>
-
-      <Table>
-        <TableHead>
-          <TableCell>Membros do Projeto</TableCell>
-          <TableCell>Etapas</TableCell>
-        </TableHead>
-        <TableRow>
-          <TableCell>{members}</TableCell>
-          <TableCell>{stages}</TableCell>
-        </TableRow>
-      </Table>
-    </>
+    <Grid sx={{ px: "10%" }}>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+          <Typography sx={{ fontSize: 16, fontWeight: "bold" }}>
+            Informações do Projeto
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Table size="small">
+            {/* PART ONE */}
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontSize: 13, color: "#777" }}>
+                  Tipo de Projeto
+                </TableCell>
+                <TableCell align="left" sx={{ fontSize: 13, color: "#777" }}>
+                  Nome do Projeto
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Typography sx={{ fontSize: 13 }}>{type}</Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <Typography sx={{ fontSize: 13 }}>{name}</Typography>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          {/* PART ONE */}
+          {/* PART ONE.HALF */}
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell align="left" sx={{ fontSize: 13, color: "#777" }}>
+                  Descrição
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell align="left">
+                  <Typography sx={{ fontSize: 13 }}>{description}</Typography>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          {/* PART ONE.HALF */}
+          {/* PART TWO */}
+          <Table size="small">
+            <TableHead sx={{ mt: 1 }}>
+              <TableRow>
+                <TableCell align="left" sx={{ fontSize: 13, color: "#777" }}>
+                  Tipo de Cliente
+                </TableCell>
+                <TableCell align="left" sx={{ fontSize: 13, color: "#777" }}>
+                  Cliente
+                </TableCell>
+                <TableCell align="left" sx={{ fontSize: 13, color: "#777" }}>
+                  Valor
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell align="left">
+                  <Typography sx={{ fontSize: 13 }}>{customerType}</Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <Typography sx={{ fontSize: 13 }}>
+                    {customer && customer.name}
+                  </Typography>
+                </TableCell>
+                <TableCell align="left">
+                  <Typography sx={{ fontSize: 13 }}>R${price}</Typography>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          {/* PART TWO */}
+          {/* PART THREE */}
+          <Table size="small">
+            <TableHead sx={{ mt: 1 }}>
+              <TableRow>
+                <TableCell sx={{ fontSize: 13, color: "#777" }}>
+                  Departamento Principal
+                </TableCell>
+                <TableCell align="left" sx={{ fontSize: 13, color: "#777" }}>
+                  Departamentos Secundários
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>
+                  <Typography sx={{ fontSize: 13 }}>
+                    {mainDepartment && mainDepartment.name}
+                  </Typography>
+                </TableCell>
+                <TableCell align="left">
+                  {selectedDepartments &&
+                    selectedDepartments.map((dept) => (
+                      <Typography key sx={{ fontSize: 13 }}>
+                        {dept.name}
+                      </Typography>
+                    ))}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+          {/* PART THREE */}
+          {/* PART FOUR */}
+          <Table size="small">
+            <TableHead sx={{ mt: 1 }}>
+              <TableRow>
+                <TableCell align="left" sx={{ fontSize: 13, color: "#777" }}>
+                  Membros do Projeto
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell align="left">
+                  <Grid container direction="row" alignItems="center">
+                    {members.map((member) => (
+                      <>
+                        <Grid item>
+                          <Avatar
+                            alt="Imagem do Colaborador"
+                            src={`http://localhost:3000/static/${member.image}`}
+                            sx={{ width: 22, height: 22 }}
+                          />
+                        </Grid>
+                        <Grid item>
+                          <Typography
+                            sx={{ ml: 0.5, mr: 1, fontSize: 13, my: "auto" }}
+                          >
+                            {member.name}
+                          </Typography>
+                        </Grid>
+                      </>
+                    ))}
+                  </Grid>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+            {/* PART FOUR */}
+          </Table>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion sx={{ my: 1 }}>
+        <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+          <Typography sx={{ fontSize: 16, fontWeight: "bold" }}>
+            Etapas e Tarefas
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Faça um map fodastico aqui</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Grid>
   );
 };
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,23 +5,26 @@ import { Typography } from "@mui/material";
 import TestWebSocketButton from "../components/small/buttons/TestWebsocketButton";
 import ProjectStageTasks from "../components/small/ProjectStageTasks";
 import TestComponent from "../components/small/TestComponent";
+import ProjectReviewTable from "../components/ProjectReviewTable";
 
 const Dashboard = ({ user }) => {
-  const fakelist = [
-    { title: "Etapa Alpha", startAt: "", dueTo: "" },
-    { title: "Etapa Beta", startAt: "", dueTo: "" },
-    { title: "Etapa Gamma", startAt: "", dueTo: "" },
-    { title: "Etapa Delta", startAt: "", dueTo: "" },
-  ];
-  const fakeColors = ["#ff5555", "#0055ff"];
+  // const fakelist = [
+  //   { title: "Etapa Alpha", startAt: "", dueTo: "" },
+  //   { title: "Etapa Beta", startAt: "", dueTo: "" },
+  //   { title: "Etapa Gamma", startAt: "", dueTo: "" },
+  //   { title: "Etapa Delta", startAt: "", dueTo: "" },
+  // ];
+  // const fakeColors = ["#ff5555", "#0055ff"];
 
   return (
+    <></>
+    // <ProjectReviewTable />
     // <TestComponent />
-    <ProjectStageTasks
-      stages={fakelist}
-      members={Array.from(user)}
-      definedStagesColors={fakeColors}
-    />
+    // <ProjectStageTasks
+    //   stages={fakelist}
+    //   members={Array.from(user)}
+    //   definedStagesColors={fakeColors}
+    // />
     // <>
     //   <TestWebSocketButton />
     //   <p>Dashboard</p>
