a5d5623ac939cfc9fd6451bd77733493f4fff8d2
cauelugli <cauec4@gmail.com>
2024-02-05 11:51:39 -0300

    return to default

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,37 +1,16 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import * as React from "react";
-import { Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import TestWebSocketButton from "../components/small/buttons/TestWebsocketButton";
-import ProjectStageTasks from "../components/small/ProjectStageTasks";
 import TestComponent from "../components/small/TestComponent";
-import ProjectReviewTable from "../components/ProjectReviewTable";
 
 const Dashboard = ({ user }) => {
-  // const fakelist = [
-  //   { title: "Etapa Alpha", startAt: "", dueTo: "" },
-  //   { title: "Etapa Beta", startAt: "", dueTo: "" },
-  //   { title: "Etapa Gamma", startAt: "", dueTo: "" },
-  //   { title: "Etapa Delta", startAt: "", dueTo: "" },
-  // ];
-  // const fakeColors = ["#ff5555", "#0055ff"];
-
   return (
-    <></>
-    // <ProjectReviewTable />
-    // <TestComponent />
-    // <ProjectStageTasks
-    //   stages={fakelist}
-    //   members={Array.from(user)}
-    //   definedStagesColors={fakeColors}
-    // />
-    // <>
-    //   <TestWebSocketButton />
-    //   <p>Dashboard</p>
-    //   <Typography sx={{ my: 3 }}>
-    //     Hellow, <strong>{user.name}</strong> !
-    //   </Typography>
-    // </>
+    <Grid>
+    {/* <TestComponent /> */}
+    {/* <TestWebSocketButton /> */}
+    </Grid>
   );
 };
 
