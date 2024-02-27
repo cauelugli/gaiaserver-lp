29f1be3d5cae47ee916f3d1d243906dcc1916272
cauelugli <cauec4@gmail.com>
2024-02-27 16:27:52 -0300

    added MyCalendar component from ReactBigCalendar

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,31 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import * as React from "react";
-import { Grid } from "@mui/material";
-import TestWebSocketButton from "../components/small/buttons/TestWebsocketButton";
-import TestComponent from "../components/small/TestComponent";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Grid,
+  Typography,
+} from "@mui/material";
+
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+import MyCalendar from "../components/MyCalender";
 
 const Dashboard = ({ user }) => {
   return (
     <Grid>
-    {/* <TestComponent /> */}
-    {/* <TestWebSocketButton /> */}
+      <Accordion sx={{mx:"20%"}}>
+        <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+          <Typography sx={{ fontSize: 22, fontWeight: "bold" }}>
+            Agenda
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <MyCalendar />
+        </AccordionDetails>
+      </Accordion>
     </Grid>
   );
 };
