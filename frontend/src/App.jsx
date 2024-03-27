6c25c85eff7cf39881f5663fbcf5d9bdc4a96267
cauelugli <cauec4@gmail.com>
2024-03-26 22:44:05 -0300

    sidebar pre hiding attempt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -93,15 +93,12 @@ export default function App() {
 
   useEffect(() => {
     socket.on("forceRefresh", () => {
-      toast.info(
-        "Atualização necessária! Recarregando a página em 10 segundos",
-        {
-          closeOnClick: false,
-          pauseOnHover: false,
-          theme: "colored",
-          autoClose: 9500,
-        }
-      );
+      toast.info("Atualização necessária! Recarregando a página em 10 segundos", {
+        closeOnClick: false,
+        pauseOnHover: false,
+        theme: "colored",
+        autoClose: 9500,
+      });
 
       const timer = setTimeout(() => {
         window.location.reload();
@@ -134,21 +131,18 @@ export default function App() {
         const resUsers = await api.get("/users");
         const resManagers = await api.get("/managers");
         const usersCombinedData = [...resUsers.data, ...resManagers.data];
-        setUsers(usersCombinedData);
+        setUsers(usersCombinedData)
 
         const resJobs = await api.get("/jobs");
         const resSales = await api.get("/sales");
         const requestsCombinedData = [...resJobs.data, ...resSales.data];
-        setRequests(requestsCombinedData);
+        setRequests(requestsCombinedData)
 
         const resCustomers = await api.get("/customers");
         const resClients = await api.get("/clients");
-        const customersCombinedData = [
-          ...resCustomers.data,
-          ...resClients.data,
-        ];
-        setCustomers(customersCombinedData);
-
+        const customersCombinedData = [...resCustomers.data, ...resClients.data];
+        setCustomers(customersCombinedData)
+        
         if (userData.hasDarkModeActive) {
           setDarkMode(true);
         }
