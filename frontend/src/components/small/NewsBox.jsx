a2fcb0ee6874ffedd8708a90c1ac1acad50d6d99
cauelugli <cauec4@gmail.com>
2024-03-31 11:35:12 -0300

    style adjustment

--- frontend/src/components/small/NewsBox.jsx
+++ frontend/src/components/small/NewsBox.jsx
@@ -5,8 +5,16 @@ import { Grid, Typography } from "@mui/material";
 
 const NewsBox = () => {
   return (
-    <Grid sx={{ height: 350, width:"auto", backgroundColor: "teal" }}>
-      <Typography>Notícias e whatever</Typography>
+    <Grid
+      sx={{
+        height: 250,
+        width: "auto",
+        backgroundColor: "rgba(44,147,147, 0.9)",
+        border: "1px solid #2c9393",
+        borderRadius:2
+      }}
+    >
+      <Typography sx={{ m: 2 }}>Notícias e Publicações</Typography>
     </Grid>
   );
 };
