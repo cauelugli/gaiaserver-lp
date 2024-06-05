50fdfe598d6738d53ea5236915e5ad45719bb80c
cauelugli <cauec4@gmail.com>
2024-06-05 14:35:53 -0300

    fixed onDelete and onUpdate behaviors on Config
    small refactor

--- api/routes/roles.js
+++ api/routes/roles.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const Config = require("../../models/models/Config");
+const Manager = require("../../models/models/Manager");
 const Role = require("../../models/models/Role");
 const User = require("../../models/models/User");
-const Manager = require("../../models/models/Manager");
 
 // GET ALL ROLES
 router.get("/", async (req, res) => {
@@ -17,16 +18,19 @@ router.get("/", async (req, res) => {
 // CREATE ROLE
 router.post("/", async (req, res) => {
   const { name } = req.body;
-  if (name === "Admin") {
+  if (name === "Admin" || name === "admin") {
     return res
       .status(420)
-      .json({ error: "Não é possível nomear para 'Admin'." });
+      .json({ error: "Não é possível nomear para 'Admin'" });
   }
+
   const existingName = await Role.findOne({ name });
   if (existingName) {
     return res.status(422).json({ error: "Nome de Perfil já cadastrado" });
   }
+
   const newRole = new Role(req.body);
+
   try {
     const savedRole = await newRole.save();
     res.status(200).json(savedRole);
@@ -41,16 +45,29 @@ router.delete("/:id", async (req, res) => {
   const roleId = req.params.id;
   try {
     const deletedRole = await Role.findByIdAndDelete(roleId);
-    const usersToUpdate = await User.find({ 'role.id': roleId });
+    const usersToUpdate = await User.find({ "role.id": roleId });
     for (const user of usersToUpdate) {
       user.role = { id: null, name: "-" };
       await user.save();
     }
-    const managersToUpdate = await Manager.find({ 'role.id': roleId });
+    const managersToUpdate = await Manager.find({ "role.id": roleId });
     for (const manager of managersToUpdate) {
       manager.role = { id: null, name: "-" };
       await manager.save();
     }
+
+    const config = await Config.findOne();
+    if (config && config.sidebar) {
+      Object.keys(config.sidebar).forEach((section) => {
+        config.sidebar[section] = config.sidebar[section].filter(
+          (item) => item._id !== roleId
+        );
+      });
+      await config.save();
+    } else {
+      console.log("\nnot found Config\n")
+    }
+
     res.status(200).json(deletedRole);
   } catch (err) {
     res.status(500).json(err);
@@ -61,10 +78,10 @@ router.delete("/:id", async (req, res) => {
 router.put("/", async (req, res) => {
   const { name, roleId } = req.body;
 
-  if (name === "Admin") {
+  if (name === "Admin" || name === "admin") {
     return res
       .status(420)
-      .json({ error: "Não é possível nomear para 'Admin'." });
+      .json({ error: "Não é possível nomear para 'Admin'" });
   }
   const existingName = await Role.findOne({ name });
 
@@ -79,15 +96,28 @@ router.put("/", async (req, res) => {
       { new: true }
     );
 
-    const usersToUpdate = await User.find({ 'role.id': roleId });
+    const usersToUpdate = await User.find({ "role.id": roleId });
     for (const user of usersToUpdate) {
       user.role = { id: roleId, name: name };
       await user.save();
     }
-    const managersToUpdate = await Manager.find({ 'role.id': roleId });
+    const managersToUpdate = await Manager.find({ "role.id": roleId });
     for (const manager of managersToUpdate) {
       manager.role = { id: roleId, name: name };
       await manager.save();
+    }
+
+    const config = await Config.findOne(); 
+    if (config && config.sidebar) {
+      Object.keys(config.sidebar).forEach(section => {
+        config.sidebar[section] = config.sidebar[section].map(item => {
+          if (item._id === roleId) {
+            return { ...item, name: name }; 
+          }
+          return item;
+        });
+      });
+      await config.save();
     }
 
     res.status(200).json(updatedRole);
