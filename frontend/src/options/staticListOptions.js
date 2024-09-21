ddb168f9017d484c7a05b1957e838084611d074b
cauelugli <cauec4@gmail.com>
2024-09-21 12:17:06 -0300

    a

--- frontend/src/options/staticListOptions.js
+++ frontend/src/options/staticListOptions.js
@@ -1,5 +1,11 @@
 // const model = ["1", "2", "3"]; // Exemplo de lista
-const status = ["Aberto", "Concluído"];
+const status = [
+  "Aberto",
+  "Em Andamento",
+  "Aguardando Cliente",
+  "Congelado",
+  "Resolvido",
+];
 
 export { status };
 // export { status, model }; // Exporta todas as listas
