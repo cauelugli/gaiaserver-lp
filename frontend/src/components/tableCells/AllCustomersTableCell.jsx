67d37ca73546a559e088ec67491cfa491863d112
cauelugli <cauec4@gmail.com>
2025-01-03 15:59:34 -0300

    added Divider and SearchBox for filtering (what a pretty component...)

--- frontend/src/components/tableCells/AllCustomersTableCell.jsx
+++ frontend/src/components/tableCells/AllCustomersTableCell.jsx
@@ -8,28 +8,40 @@ const api = axios.create({
 
 import {
   Avatar,
+  Divider,
   Grid,
   InputLabel,
   MenuItem,
   Select,
+  TextField,
   Typography,
+  InputAdornment,
 } from "@mui/material";
 
 const AllCustomersTableCell = (props) => {
-  const [options, setOptions] = React.useState([]);
+  const [customers, setCustomers] = React.useState([]);
+  const [clients, setClients] = React.useState([]);
+  const [searchQuery, setSearchQuery] = React.useState("");
 
   React.useEffect(() => {
     const fetchData = async () => {
       try {
-        let data = [];
         const resCustomers = await api.get("/get", {
           params: { model: "Customer" },
         });
         const resClients = await api.get("/get", {
           params: { model: "Client" },
         });
-        data = [...resCustomers.data, ...resClients.data];
-        setOptions(data);
+
+        const sortedCustomers = resCustomers.data.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+        const sortedClients = resClients.data.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+
+        setCustomers(sortedCustomers);
+        setClients(sortedClients);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -37,26 +49,12 @@ const AllCustomersTableCell = (props) => {
     fetchData();
   }, [props.field.dynamicData, props.fields.data]);
 
-  const renderValue = (selected) => {
-    if (!selected) {
-      return <Typography>{props.field.label}</Typography>;
-    } else {
-      return (
-        <>
-          {
-            <Grid container direction="row" alignItems="center">
-              <Avatar
-                alt="Imagem"
-                src={`http://localhost:3000/static/${selected.image}`}
-                sx={{ width: 24, height: 24, marginRight: 2 }}
-              />
-              <Typography sx={{ fontSize: 13 }}>{selected.name}</Typography>
-            </Grid>
-          }
-        </>
-      );
-    }
-  };
+  const filteredCustomers = customers.filter((customer) =>
+    customer.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  const filteredClients = clients.filter((client) =>
+    client.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
     <>
@@ -64,32 +62,103 @@ const AllCustomersTableCell = (props) => {
       <Select
         value={props.fields[props.field.name]}
         onChange={props.handleChange(props.field.name)}
-        sx={{
-          width:
-            props.modalOptions.maxWidth === "xs"
-              ? 190
-              : props.modalOptions.maxWidth === "sm"
-              ? 175
-              : props.modalOptions.maxWidth === "md"
-              ? 200
-              : 200,
-          minWidth: 175,
-        }}
+        sx={{ minWidth: 175, width: 200, maxHeight: 500 }}
         size="small"
-        renderValue={renderValue}
-      >
-        {options.map((option, index) => (
-          <MenuItem value={option} key={index}>
+        renderValue={(selected) =>
+          selected ? (
             <Grid container direction="row" alignItems="center">
               <Avatar
                 alt="Imagem"
-                src={`http://localhost:3000/static/${option.image}`}
+                src={`http://localhost:3000/static/${selected.image}`}
                 sx={{ width: 24, height: 24, marginRight: 2 }}
               />
-              <Typography>{option.name}</Typography>
+              <Typography>{selected.name}</Typography>
             </Grid>
-          </MenuItem>
-        ))}
+          ) : (
+            <Typography>Selecione um Cliente</Typography>
+          )
+        }
+        MenuProps={{ PaperProps: { sx: { padding: "10px" } } }}
+      >
+        <TextField
+          placeholder="Buscar..."
+          variant="outlined"
+          size="small"
+          fullWidth
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={(e) => e.stopPropagation()}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                🔍 {/* Ícone de busca */}
+              </InputAdornment>
+            ),
+          }}
+          sx={{
+            marginBottom: 2,
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "4px",
+              fontSize: "13px",
+              height: "30px",
+            },
+          }}
+        />
+        {filteredCustomers.length !== 0 && (
+          <InputLabel sx={{ ml: 1, fontSize: 13 }}>Empresas</InputLabel>
+        )}
+        {filteredCustomers.length === 0 ? (
+          filteredClients.length === 0 ? (
+            <Typography sx={{ ml: 1, fontSize: 13 }}>
+              Nenhum Cliente Encontrada
+            </Typography>
+          ) : (
+            <Typography sx={{ ml: 1, fontSize: 13 }}>
+              Nenhuma Empresa Encontrada
+            </Typography>
+          )
+        ) : (
+          filteredCustomers.map((option, index) => (
+            <MenuItem value={option} key={index}>
+              <Grid container direction="row" alignItems="center">
+                <Avatar
+                  alt="Imagem"
+                  src={`http://localhost:3000/static/${option.image}`}
+                  sx={{ width: 24, height: 24, marginRight: 2 }}
+                />
+                <Typography>{option.name}</Typography>
+              </Grid>
+            </MenuItem>
+          ))
+        )}
+        <Divider
+          sx={{ border: "1px nothing #444", width: "75%", mx: "auto", my: 1 }}
+        />
+        {filteredClients.length !== 0 && (
+          <InputLabel sx={{ ml: 1, fontSize: 13 }}>Pessoa Física</InputLabel>
+        )}
+        {filteredClients.length === 0 ? (
+          filteredCustomers.length === 0 ? (
+            ""
+          ) : (
+            <Typography sx={{ ml: 1, fontSize: 13 }}>
+              Nenhum Cliente Encontrado
+            </Typography>
+          )
+        ) : (
+          filteredClients.map((option, index) => (
+            <MenuItem value={option} key={index}>
+              <Grid container direction="row" alignItems="center">
+                <Avatar
+                  alt="Imagem"
+                  src={`http://localhost:3000/static/${option.image}`}
+                  sx={{ width: 24, height: 24, marginRight: 2 }}
+                />
+                <Typography>{option.name}</Typography>
+              </Grid>
+            </MenuItem>
+          ))
+        )}
       </Select>
     </>
   );
