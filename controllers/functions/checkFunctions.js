2899b8ad57e24042f52f6713b36b1335404e4b0d
cauelugli <cauec4@gmail.com>
2025-01-10 15:49:34 -0300

    removed adding and removing from check functions (they only _check_ bro)

--- controllers/functions/checkFunctions.js
+++ controllers/functions/checkFunctions.js
@@ -1,21 +1,15 @@
 const Config = require("../../models/models/Config");
 
-const { removeFromStock } = require("./deleteFunctions");
-
-async function checkNewRequestDefaultStatus(fields, selectedProducts) {
+async function checkNewRequestDefaultStatus(fields) {
   try {
     const config = await Config.findOne();
-
     if (config.requests.requestsNeedApproval === false) {
       fields.status = "Aprovado";
-      if (selectedProducts && Array.isArray(selectedProducts)) {
-        await removeFromStock(selectedProducts);
-      }
     } else {
       fields.status = "Aberto";
     }
   } catch (err) {
-    console.error("Erro ao verificar requestsNeedApproval", err);
+    console.error("Erro ao verificar checkNewRequestDefaultStatus");
     throw err;
   }
 }
@@ -25,14 +19,11 @@ async function checkNewStockEntryDefaultStatus(fields) {
     const config = await Config.findOne();
     if (config.stock.stockEntriesNeedApproval === false) {
       fields.status = "Aprovado";
-      if (fields.items && Array.isArray(fields.items)) {
-        await addToStock(fields.items);
-      }
     } else {
       fields.status = "Aberto";
     }
   } catch (err) {
-    console.error("Erro ao verificar stockEntriesNeedApproval:", err);
+    console.error("Erro ao verificar checkNewStockEntryDefaultStatus");
     throw err;
   }
 }
