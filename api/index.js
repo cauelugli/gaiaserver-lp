b678d0af450fb07bb3c66bc3ca7451ac196ef129
cauelugli <cauec4@gmail.com>
2025-01-21 15:16:45 -0300

    created routeOptions to refactor index.js on API

--- api/index.js
+++ api/index.js
@@ -6,23 +6,7 @@ const server = http.createServer(app);
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const addRoute = require("./routes/add");
-const editRoute = require("./routes/edit");
-const editSmallRoute = require("./routes/editSmall");
-const deleteRoute = require("./routes/delete");
-const getRoute = require("./routes/get");
-const notificationsRoute = require("./routes/notifications");
-const getConfigRoute = require("./routes/getConfig");
-const idIndexListRoute = require("./routes/idIndexList");
-
-const actionsRoute = require("./routes/actions");
-const adminRoute = require("./routes/admin");
-const agendaRoute = require("./routes/agenda");
-const authRoute = require("./routes/auth");
-const configRoute = require("./routes/config");
-const uploadsRoute = require("./routes/uploads");
-const userPreferencesRoute = require("./routes/userPreferences");
-const productsRoute = require("./routes/products");
+const routes = require("./routeOptions");
 
 dotenv.config();
 app.use(cors());
@@ -36,27 +20,12 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(console.log("Connected to MongoDB"))
+  .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-app.use("/api/actions", actionsRoute);
-app.use("/api/add", addRoute);
-app.use("/api/edit", editRoute);
-app.use("/api/editSmall", editSmallRoute);
-app.use("/api/delete", deleteRoute);
-app.use("/api/get", getRoute);
-app.use("/api/notifications", notificationsRoute);
-app.use("/api/idIndexList", idIndexListRoute);
-app.use("/api/admin", adminRoute);
-app.use("/api/getConfig", getConfigRoute);
-app.use("/api/auth", authRoute);
-app.use("/api/login", authRoute);
-app.use("/api/config", configRoute);
-app.use("/api/products", productsRoute);
-
-app.use("/api/uploads", uploadsRoute);
-app.use("/api/agenda", agendaRoute);
-app.use("/api/userPreferences", userPreferencesRoute);
+routes.forEach(({ path, route }) => {
+  app.use(path, route);
+});
 
 server.listen(3000, () => {
   console.log("Backend is running.");
---
+++ api/routeOptions.js
@@ -0,0 +1,38 @@
+const addRoute = require("./routes/add");
+const editRoute = require("./routes/edit");
+const editSmallRoute = require("./routes/editSmall");
+const deleteRoute = require("./routes/delete");
+const getRoute = require("./routes/get");
+const notificationsRoute = require("./routes/notifications");
+const getConfigRoute = require("./routes/getConfig");
+const idIndexListRoute = require("./routes/idIndexList");
+const actionsRoute = require("./routes/actions");
+const adminRoute = require("./routes/admin");
+const agendaRoute = require("./routes/agenda");
+const authRoute = require("./routes/auth");
+const configRoute = require("./routes/config");
+const uploadsRoute = require("./routes/uploads");
+const userPreferencesRoute = require("./routes/userPreferences");
+const productsRoute = require("./routes/products");
+
+const routes = [
+  { path: "/api/actions", route: actionsRoute },
+  { path: "/api/add", route: addRoute },
+  { path: "/api/edit", route: editRoute },
+  { path: "/api/editSmall", route: editSmallRoute },
+  { path: "/api/delete", route: deleteRoute },
+  { path: "/api/get", route: getRoute },
+  { path: "/api/notifications", route: notificationsRoute },
+  { path: "/api/idIndexList", route: idIndexListRoute },
+  { path: "/api/admin", route: adminRoute },
+  { path: "/api/getConfig", route: getConfigRoute },
+  { path: "/api/auth", route: authRoute },
+  { path: "/api/login", route: authRoute },
+  { path: "/api/config", route: configRoute },
+  { path: "/api/products", route: productsRoute },
+  { path: "/api/uploads", route: uploadsRoute },
+  { path: "/api/agenda", route: agendaRoute },
+  { path: "/api/userPreferences", route: userPreferencesRoute },
+];
+
+module.exports = routes;
