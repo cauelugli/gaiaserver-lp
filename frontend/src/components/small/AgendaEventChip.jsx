1fa61aa82c863937dd76d79e7aab7c604b0d80df
cauelugli <cauec4@gmail.com>
2025-02-04 20:33:34 -0300

    AgendaEventChip enhancement

--- frontend/src/components/small/AgendaEventChip.jsx
+++ frontend/src/components/small/AgendaEventChip.jsx
@@ -2,29 +2,66 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 
-import { Grid, Typography } from "@mui/material";
+import { useAppData } from "../../AppDataContext";
+
+import { Avatar, Grid, Tooltip, Typography } from "@mui/material";
 
 const AgendaEventChip = (props) => {
+  const appData = useAppData();
+  const idIndexList = appData.idIndexList;
+
   return (
     <Grid
       container
       direction="row"
       alignItems="center"
-      justifyContent="flex-start"
+      justifyContent="center"
       sx={{
-        width: "80%",
+        width: "95%",
         border: "1px solid #c5c5cc",
         borderRadius: 3,
         p: 1,
         mx: "auto",
         mt: 1,
       }}
-      gap={4}
+      gap={2}
     >
-      <Typography sx={{ fontSize: 11, color:"black" }}>{"o"}</Typography>
-      <Typography sx={{ fontSize: 11, color:"black" }}>{props.item.service}</Typography>
-      <Typography sx={{ fontSize: 11, color:"black" }}>{props.item.scheduleTime}</Typography>
-      <Typography sx={{ fontSize: 11, color:"black" }}>{"+"}</Typography>
+      <Tooltip
+        title={
+          idIndexList.find((item) => item.id === props.item.customer)?.name ||
+          ""
+        }
+      >
+        <Avatar
+          src={`http://localhost:3000/static/${
+            idIndexList.find((item) => item.id === props.item.customer)
+              ?.image || ""
+          }`}
+          sx={{ width: 16, height: 16, mr: 1 }}
+        />
+      </Tooltip>
+      <Tooltip
+        title={
+          idIndexList.find((item) => item.id === props.item.service)?.name || ""
+        }
+      >
+        <Typography
+          sx={{
+            fontSize: 11,
+            color: "black",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            maxWidth: 100,
+          }}
+        >
+          {idIndexList.find((item) => item.id === props.item.service)?.name ||
+            ""}
+        </Typography>
+      </Tooltip>
+      <Typography sx={{ fontSize: 11, color: "black" }}>
+        {props.item.scheduleTime}
+      </Typography>
     </Grid>
   );
 };
