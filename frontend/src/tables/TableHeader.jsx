079bcbcbbd61431329e76e01cb8ad39c1891fb89
cauelugli <cauec4@gmail.com>
2025-02-27 16:24:41 -0300

    trying to fix break...

--- frontend/src/tables/TableHeader.jsx
+++ frontend/src/tables/TableHeader.jsx
@@ -12,31 +12,35 @@ function TableHeader(props) {
         },
       }}
     >
-      {props.tableColumns[props.itemIndex].map((headCell, cellIndex) => (
-        <TableCell
-          key={cellIndex}
-          align={cellIndex === 0 ? "" : "left"}
-          sx={{
-            fontSize: 13,
-            fontWeight: "bold",
-            width:
-              cellIndex === 0
-                ? 50
-                : cellIndex === 1 && headCell.id === "type"
-                ? 50
-                : "auto",
-          }}
-          sortDirection={props.orderBy === headCell.id ? props.order : false}
-        >
-          <TableSortLabel
-            active={props.orderBy === headCell.id}
-            direction={props.orderBy === headCell.id ? props.order : "asc"}
-            onClick={() => props.handleRequestSort(headCell.id)}
-          >
-            {headCell.label}
-          </TableSortLabel>
-        </TableCell>
-      ))}
+      {props && props.tableColumns
+        ? props.tableColumns[props.itemIndex].map((headCell, cellIndex) => (
+            <TableCell
+              key={cellIndex}
+              align={cellIndex === 0 ? "" : "left"}
+              sx={{
+                fontSize: 13,
+                fontWeight: "bold",
+                width:
+                  cellIndex === 0
+                    ? 50
+                    : cellIndex === 1 && headCell.id === "type"
+                    ? 50
+                    : "auto",
+              }}
+              sortDirection={
+                props.orderBy === headCell.id ? props.order : false
+              }
+            >
+              <TableSortLabel
+                active={props.orderBy === headCell.id}
+                direction={props.orderBy === headCell.id ? props.order : "asc"}
+                onClick={() => props.handleRequestSort(headCell.id)}
+              >
+                {headCell.label}
+              </TableSortLabel>
+            </TableCell>
+          ))
+        : ""}
       <TableCell align="right" sx={{ fontSize: 13, fontWeight: "bold" }}>
         Ações
       </TableCell>
