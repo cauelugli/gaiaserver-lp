388eddd72a75048b2b697359743ae7c6fc91b5c1
cauelugli <cauec4@gmail.com>
2025-03-27 15:31:32 -0300

    fixed stuff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
     allowedHosts: ["gaiaserver"],
     host: "0.0.0.0",
     port: 5173,
+    strictPort: true, // Adicione isso para evitar fallback
+    hmr: {
+      clientPort: 5173, // Importante para Docker
+    },
   },
   build: {
     target: "esnext", // Adiciona suporte a top-level await
