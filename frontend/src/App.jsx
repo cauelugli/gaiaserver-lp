193d4566ba822f9638cf21c69b247c0f4fe98597
cauelugli <cauec4@gmail.com>
2025-04-10 13:52:19 -0300

    added first layout to home page and lint some files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import { Button, Typography, Box } from '@mui/material';
+import { Box } from "@mui/material";
+
+import Features from "./components/Features";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import LogoCollection from "./components/LogoCollection";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <Box sx={{ textAlign: 'center', marginTop: 4 }}>
-      <Typography variant="h2">Hello World com Vite!</Typography>
-      <Button variant="contained" sx={{ marginTop: 2 }}>
-        Material UI Button
-      </Button>
+    <Box sx={{ textAlign: "center", marginTop: 4 }}>
+      <Navbar />
+      <Hero />
+      <LogoCollection />
+      <Features />
+      <Testimonials />
+      <Pricing />
+      <Footer />
     </Box>
   );
 }
