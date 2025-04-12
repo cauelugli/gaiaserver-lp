176f510d1857d6fcb6ba708f6acbf4f9d681f327
cauelugli <cauec4@gmail.com>
2025-04-12 14:16:12 -0300

    a

--- frontend/src/components/large/Features.jsx
+++ frontend/src/components/large/Features.jsx
@@ -1,8 +1,10 @@
 import * as React from "react";
+
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+
 import Carousel from "../small/Carousel";
 import featureOptions from "../../options/featureOptions";
 
